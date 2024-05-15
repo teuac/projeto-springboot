e489971ad5c418ff344876e5e37861924b1d120a
teuac <cauetcampos@outlook.com>
2024-05-15 15:45:42 -0300

    order status enum

--- src/main/java/com/udemy/course/config/TesteConfig.java
+++ src/main/java/com/udemy/course/config/TesteConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.udemy.course.entities.Order;
 import com.udemy.course.entities.User;
+import com.udemy.course.entities.enums.OrderStatus;
 import com.udemy.course.repositories.OrderRepository;
 import com.udemy.course.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TesteConfig implements CommandLineRunner {
 		User u1  = new User(null, "Maria Brown", "maria@gmail.com", "9999999999", "2223323");
 		User u2  = new User(null, "Pedro White", "pedro@hotmail.com", "8888888888", "12345567890");
 		
-		Order o1 = new Order(null, Instant.parse("2020-06-19T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2020-06-20T15:08:52Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2020-06-21T20:32:45Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2020-06-19T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2020-06-20T15:08:52Z"),OrderStatus.SHIPPED, u2);
+		Order o3 = new Order(null, Instant.parse("2020-06-21T20:32:45Z"),OrderStatus.DELIVERED, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/udemy/course/entities/Order.java
+++ src/main/java/com/udemy/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.udemy.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,6 +28,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -35,11 +38,12 @@ public class Order implements Serializable {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
@@ -56,6 +60,15 @@ public class Order implements Serializable {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+	
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOF(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus.getCode();
 	}
 
 	public User getClient() {
