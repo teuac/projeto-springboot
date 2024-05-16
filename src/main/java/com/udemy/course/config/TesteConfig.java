3345795549afc751044fc73ba87dc6631aa144a9
teuac <cauetcampos@outlook.com>
2024-05-15 21:10:13 -0300

    payment OneToOne association

--- src/main/java/com/udemy/course/config/TesteConfig.java
+++ src/main/java/com/udemy/course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.udemy.course.entities.Category;
 import com.udemy.course.entities.Order;
 import com.udemy.course.entities.OrderItem;
+import com.udemy.course.entities.Payment;
 import com.udemy.course.entities.Product;
 import com.udemy.course.entities.User;
 import com.udemy.course.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TesteConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2020-06-19T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
