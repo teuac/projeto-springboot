0498a32bd6f10a8eee3a479bef7daeb2e663cabe
teuac <cauetcampos@outlook.com>
2024-05-17 18:08:48 -0300

    Exception Handling - delete

--- src/main/java/com/udemy/course/services/UserService.java
+++ src/main/java/com/udemy/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.course.entities.User;
 import com.udemy.course.repositories.UserRepository;
+import com.udemy.course.services.exceptions.DatabaseException;
 import com.udemy.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +33,15 @@ public class UserService{
 	}
 	
 	public void delete(Long id) {
+		try {
 		 repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(RuntimeException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User update(Long id, User user) {
 		User entity = repository.getReferenceById(id);
